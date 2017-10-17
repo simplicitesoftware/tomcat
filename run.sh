fd42df27cf15e69e546f5a8f01063f23386787de
David AZOULAY <dazoulay@simplicite.fr>
2017-10-17 18:32:32 +0200

    Updated

--- run.sh
+++ run.sh
@@ -9,46 +9,52 @@ echo "Tomcat root: $TOMCAT_ROOT"
 export JAVA_OPTS="$JAVA_OPTS -server -Dfile.encoding=UTF-8 -Dgit.basedir=$TOMCAT_ROOT/webapps/ROOT/WEB-INF/git -Dplatform.autoupgrade=true"
 export JAVA_OPTS="$JAVA_OPTS -Dtomcat.adminport=${TOMCAT_ADMIN_PORT:-8005} -Dtomcat.httpport=${TOMCAT_HTTP_PORT:-8080} -Dtomcat.httpsport=${TOMCAT_HTTPS_PORT:-8443}"
 
-[ "$DB_VENDOR" = "" ] && DB_VENDOR=hsqldb
-[ "$DB_VENDOR" = "mariadb" ] && DB_VENDOR=mysql
-[ "$DB_VENDOR" = "pgsql" ] && DB_VENDOR=postgresql
-echo "Database vendor: $DB_VENDOR"
-
-if [ $DB_VENDOR = "mysql" ]
+if [ -d $TOMCAT_ROOT/webapps/ROOT ]
 then
-	echo "exit" | mysql --silent --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --database=$DB_NAME
-	RET=$?
-	if [ $RET -ne 0 ]
+	[ "$DB_VENDOR" = "" ] && DB_VENDOR=hsqldb
+	[ "$DB_VENDOR" = "mariadb" ] && DB_VENDOR=mysql
+	[ "$DB_VENDOR" = "pgsql" ] && DB_VENDOR=postgresql
+	echo "Database vendor: $DB_VENDOR"
+	
+	if [ $DB_VENDOR = "mysql" ]
 	then
-		echo "Unable to connect to database $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
-		exit 1
-	fi
-	EXISTS=`echo "show tables like 'm_system'" | mysql --silent --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --database=$DB_NAME`
-	if [ "$EXISTS" = "" ]
-	then
-		if [ -f $TOMCAT_ROOT/webapps/ROOT/WEB-INF/db/simplicite-mysql.dmp ]
+		echo "exit" | mysql --silent --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --database=$DB_NAME
+		RET=$?
+		if [ $RET -ne 0 ]
 		then
-			echo "Loading database $DB_VENDOR / $DM_HOST / $DB_PORT / $DB_USER..."
-			mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD < $TOMCAT_ROOT/webapps/ROOT/WEB-INF/db/simplicite-mysql.dmp
-			RET=$?
-			if [ $RET -ne 0 ]
-			then
-				echo "Load database error on $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
-				exit 3
-			fi
-			echo "Done"
-		else
-			echo "No dump to load database $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
-			exit 2
+			echo "Unable to connect to database $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
+			exit 1
 		fi
+		EXISTS=`echo "show tables like 'm_system'" | mysql --silent --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD --database=$DB_NAME`
+		if [ "$EXISTS" = "" ]
+		then
+			if [ -f $TOMCAT_ROOT/webapps/ROOT/WEB-INF/db/simplicite-mysql.dmp ]
+			then
+				echo "Loading database $DB_VENDOR / $DM_HOST / $DB_PORT / $DB_USER..."
+				mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASSWORD < $TOMCAT_ROOT/webapps/ROOT/WEB-INF/db/simplicite-mysql.dmp
+				RET=$?
+				if [ $RET -ne 0 ]
+				then
+					echo "Load database error on $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
+					exit 3
+				fi
+				echo "Done"
+			else
+				echo "No dump to load database $DB_VENDOR / $DB_HOST / $DB_PORT / $DB_USER" >&2
+				exit 2
+			fi
+		fi
+		sed -i 's/<!-- hsqldb --><Resource/<!-- hsqldb --><!-- Resource/;s/<\/Resource><!-- hsqldb -->/<\/Resource --><!-- hsqldb -->/;s/<!-- mysql --><!-- Resource/<!-- mysql --><Resource/;s/<\/Resource --><!-- mysql -->/<\/Resource><!-- mysql -->/' $TOMCAT_ROOT/webapps/ROOT/META-INF/context.xml
+		JAVA_OPTS="$JAVA_OPTS -Dmysql.user=$DB_USER -Dmysql.password=$DB_PASSWORD -Dmysql.host=$DB_HOST -Dmysql.port=$DB_PORT -Dmysql.database=$DB_NAME"
+	elif [ $DB_VENDOR = "postgresql" ]
+	then
+		# TODO: check database and load dump if empty (see above)
+		sed -i 's/<!-- hsqldb --><Resource/<!-- hsqldb --><!-- Resource/;s/<\/Resource><!-- hsqldb -->/<\/Resource --><!-- hsqldb -->/;s/<!-- postgressql --><!-- Resource/<!-- postgresql --><Resource/;s/<\/Resource --><!-- postgresql -->/<\/Resource><!-- postgresql -->/' $TOMCAT_ROOT/webapps/ROOT/META-INF/context.xml
+		JAVA_OPTS="$JAVA_OPTS -Dpostgresql.user=$DB_USER -Dpostgresql.password=$DB_PASSWORD -Dpostgresql.host=$DB_HOST -Dpostgresql.port=$DB_PORT -Dpostgresql.database=$DB_NAME"
 	fi
-	sed -i 's/<!-- hsqldb --><Resource/<!-- hsqldb --><!-- Resource/;s/<\/Resource><!-- hsqldb -->/<\/Resource --><!-- hsqldb -->/;s/<!-- mysql --><!-- Resource/<!-- mysql --><Resource/;s/<\/Resource --><!-- mysql -->/<\/Resource><!-- mysql -->/' $TOMCAT_ROOT/webapps/ROOT/META-INF/context.xml
-	JAVA_OPTS="$JAVA_OPTS -Dmysql.user=$DB_USER -Dmysql.password=$DB_PASSWORD -Dmysql.host=$DB_HOST -Dmysql.port=$DB_PORT -Dmysql.database=$DB_NAME"
-elif [ $DB_VENDOR = "postgresql" ]
-then
-	# TODO: check database and load dump if empty (see above)
-	sed -i 's/<!-- hsqldb --><Resource/<!-- hsqldb --><!-- Resource/;s/<\/Resource><!-- hsqldb -->/<\/Resource --><!-- hsqldb -->/;s/<!-- postgressql --><!-- Resource/<!-- postgresql --><Resource/;s/<\/Resource --><!-- postgresql -->/<\/Resource><!-- postgresql -->/' $TOMCAT_ROOT/webapps/ROOT/META-INF/context.xml
-	JAVA_OPTS="$JAVA_OPTS -Dpostgresql.user=$DB_USER -Dpostgresql.password=$DB_PASSWORD -Dpostgresql.host=$DB_HOST -Dpostgresql.port=$DB_PORT -Dpostgresql.database=$DB_NAME"
+else
+	mkdir $TOMCAT_ROOT/webapps/ROOT
+	echo "It works!" > $TOMCAT_ROOT/webapps/ROOT/index.jsp
 fi
 
 echo "Java options: $JAVA_OPTS"
