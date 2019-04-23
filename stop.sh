ad697ed2b0cd929789bf0ed7dc7b607f44da634d
David AZOULAY <dazoulay@simplicite.fr>
2019-04-23 15:46:03 +0200

    Stop.sh

---
+++ stop.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+[ "$JAVA_HOME" = "" ] && JAVA_HOME="/usr/lib/jvm/java"
+if [ ! -d $JAVA_HOME ]
+then
+	echo "JAVA_HOME = $JAVA_HOME is not correctly configured" >&2
+	exit 1
+fi
+export PATH=$JAVA_HOME/bin:$PATH
+
+[ "$TOMCAT_ROOT" = "" ] && TOMCAT_ROOT=`dirname $0`
+TOMCAT_ROOT=`realpath $TOMCAT_ROOT`
+echo "Tomcat root: $TOMCAT_ROOT"
+
+export JAVA_OPTS="$JAVA_OPTS -server -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Dtomcat.adminport=${TOMCAT_ADMIN_PORT:-8005}"
+
+echo "Java options: $JAVA_OPTS"
+
+cd $TOMCAT_ROOT/bin
+./shutdown.sh
+cd ..
+
+if [ "$1" = "-t" ]
+then
+	LOG=logs/catalina.out
+	while [ ! -f $LOG ]; do echo -n "."; sleep 1; done
+	tail -f $LOG
+fi
+
+exit 0
