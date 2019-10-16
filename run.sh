09d9a1f673887c0990e21ab9e3a003de236e3e43
David AZOULAY <dazoulay@simplicite.fr>
2019-10-16 23:12:03 +0200

    Updated

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ echo "Tomcat root: $TOMCAT_ROOT"
 
 export JAVA_OPTS="$JAVA_OPTS -server -Djava.awt.headless=true -Dfile.encoding=UTF-8 -Duser.timezone=${TOMCAT_TIMEZONE:-`date +%Z`} -Dplatform.autoupgrade=true"
 export JAVA_OPTS="$JAVA_OPTS -Dtomcat.adminport=${TOMCAT_ADMIN_PORT:-8005} -Dtomcat.httpport=${TOMCAT_HTTP_PORT:-8080} -Dtomcat.httpsport=${TOMCAT_HTTPS_PORT:-8443}"
-[ "$SSL" = "true" -o ${TOMCAT_SSL_PORT:-0} -gt 0 ] && export JAVA_OPTS="$JAVA_OPTS -Dtomcat.sslport=${TOMCAT_SSL_PORT:-8483} -Dtomcat.sslkeystorefile=${KEYSTORE_FILE:-conf/server.jks} -Dtomcat.sslkeystorepassword=$_KEYSTORE_PASSWORD:-password}"
+[ "$SSL" = "true" -o ${TOMCAT_SSL_PORT:-0} -gt 0 ] && export JAVA_OPTS="$JAVA_OPTS -Dtomcat.sslport=${TOMCAT_SSL_PORT:-8444} -Dtomcat.sslkeystorefile=${KEYSTORE_FILE:-conf/server.jks} -Dtomcat.sslkeystorepassword=$_KEYSTORE_PASSWORD:-password}"
 [ "$AJP" = "true" -o ${TOMCAT_AJP_PORT:-0} -gt 0 ] && export JAVA_OPTS="$JAVA_OPTS -Dtomcat.ajpport=${TOMCAT_AJP_PORT:-8009}"
 export JAVA_OPTS="$JAVA_OPTS -Dgit.basedir=${GIT_BASEDIR:-$TOMCAT_ROOT/webapps/ROOT/WEB-INF/git}"
 
@@ -31,8 +31,8 @@ if [ -d $TOMCAT_ROOT/webapps/ROOT ]
 then
 	LOG4J="$TOMCAT_ROOT/webapps/ROOT/WEB-INF/classes/log4j.xml"
 	[ -f $LOG4J ] && sed -i 's/<!-- appender-ref ref="SIMPLICITE-CONSOLE"\/ -->/<appender-ref ref="SIMPLICITE-CONSOLE"\/>/' $LOG4J
-	[ ${TOMCAT_SSL_PORT:-0} -gt 0 ] && sed -i 's/<!-- SSL Connector/<Connector/;s/Connector SSL -->/Connector>/' $TOMCAT_ROOT/conf/server.xml
-	[ ${TOMCAT_AJP_PORT:-0} -gt 0 ] && sed -i 's/<!-- AJP Connector/<Connector/;s/Connector AJP -->/Connector>/' $TOMCAT_ROOT/conf/server.xml
+	[ "$SSL" = "true" -o ${TOMCAT_SSL_PORT:-0} -gt 0 ] && sed -i 's/<!-- SSL Connector/<Connector/;s/Connector SSL -->/Connector>/' $TOMCAT_ROOT/conf/server.xml
+	[ "$AJP" = "true" -o ${TOMCAT_AJP_PORT:-0} -gt 0 ] && sed -i 's/<!-- AJP Connector/<Connector/;s/Connector AJP -->/Connector>/' $TOMCAT_ROOT/conf/server.xml
 	[ "$DB_VENDOR" = "" ] && DB_VENDOR=hsqldb
 	[ "$DB_VENDOR" = "mariadb" ] && DB_VENDOR=mysql
 	[ "$DB_VENDOR" = "pgsql" -o "$DB_VENDOR" = "postgres" ] && DB_VENDOR=postgresql
