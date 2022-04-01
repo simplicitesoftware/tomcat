199fa08eba45ec1608316665ff0552013e42b744
David AZOULAY <dazoulay@simplicite.fr>
2022-04-01 13:17:08 +0200

    Version 9.0.62

--- bin/service.bat
+++ bin/service.bat
@@ -209,7 +209,7 @@ if exist "%CATALINA_HOME%\bin\%DEFAULT_SERVICE_NAME%.exe" (
 )
 
 "%EXECUTABLE%" //IS//%SERVICE_NAME% ^
-    --Description "Apache Tomcat 9.0.60 Server - https://tomcat.apache.org/" ^
+    --Description "Apache Tomcat 9.0.62 Server - https://tomcat.apache.org/" ^
     --DisplayName "Apache Tomcat 9.0 %SERVICE_NAME%" ^
     --Install "%EXECUTABLE%" ^
     --LogPath "%CATALINA_BASE%\logs" ^
