0712d80704e5fae02348c57083c3da8eaa67d78a
David AZOULAY <dazoulay@simplicite.fr>
2024-03-26 09:40:46 +0100

    Tomcat version 10.1.20

--- bin/service.bat
+++ bin/service.bat
@@ -201,7 +201,7 @@ if exist "%CATALINA_HOME%\bin\%DEFAULT_SERVICE_NAME%.exe" (
 )
 
 "%EXECUTABLE%" //IS//%SERVICE_NAME% ^
-    --Description "Apache Tomcat 10.1.19 Server - https://tomcat.apache.org/" ^
+    --Description "Apache Tomcat 10.1.20 Server - https://tomcat.apache.org/" ^
     --DisplayName "Apache Tomcat 10.1 %SERVICE_NAME%" ^
     --Install "%EXECUTABLE%" ^
     --LogPath "%CATALINA_BASE%\logs" ^
