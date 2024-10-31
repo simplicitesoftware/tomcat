8ca10a2ca53b738c5467ad1f595859aae8251890
David AZOULAY <dazoulay@simplicite.fr>
2024-10-31 14:40:42 +0100

    JaCoCo report scope

--- jacocoreport.sh
+++ jacocoreport.sh
@@ -21,11 +21,24 @@ then
 		JCCREPORTDIR=${JACOCO_REPORTDIR:-${TOMCAT_ROOT}/webapps/jacoco}
 		[ ! -d $JCCREPORTDIR ] && mkdir -p $JCCREPORTDIR
 		rm -fr $JCCREPORTDIR/*
+
+		SRC=""
+		CLS=""
+		for MODULE in ${JACOCO_MODULES//,/ }
+		do
+			# Avoid including tests
+			for PKG in commons objects extobjects workflows dispositions adapters
+			do
+				SRC="$SRC --sourcefiles ${TOMCAT_ROOT}/webapps/${TOMCAT_WEBAPP:-ROOT}/WEB-INF/src/com/simplicite/$PKG/$MODULE"
+				CLS="$CLS --classfiles ${TOMCAT_ROOT}/webapps/${TOMCAT_WEBAPP:-ROOT}/WEB-INF/bin/com/simplicite/$PKG/$MODULE"
+			done
+		done
+
 		java -jar ${JCCHOME}/jacococli.jar \
 			report ${JCCDESTFILE} \
 			--html ${JCCREPORTDIR} \
-			--sourcefiles ${TOMCAT_ROOT}/webapps/${TOMCAT_WEBAPP:-ROOT}/WEB-INF/src \
-			--classfiles ${TOMCAT_ROOT}/webapps/${TOMCAT_WEBAPP:-ROOT}/WEB-INF/bin
+			$SRC \
+			$CLS
 	else
 		echo "Warning: JaCoCo exec file does not exists"
 		exit 3
