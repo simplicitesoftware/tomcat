4ca6a1c8f36b73c26bee885ef16ad4d2f99ea6ea
David AZOULAY <dazoulay@simplicite.fr>
2026-04-06 21:27:51 +0200

    Tomcat version 11.0.21

--- bin/service.bat
+++ bin/service.bat
@@ -210,7 +210,7 @@ set "JVM9_OPTIONS=%JVM9_OPTIONS%;--add-opens=java.rmi/sun.rmi.transport=ALL-UNNA
 set "JVM9_OPTIONS=%JVM9_OPTIONS%;--enable-native-access=ALL-UNNAMED"
 
 "%EXECUTABLE%" //IS//%SERVICE_NAME% ^
-    --Description "Apache Tomcat 11.0.20 Server - https://tomcat.apache.org/" ^
+    --Description "Apache Tomcat 11.0.21 Server - https://tomcat.apache.org/" ^
     --DisplayName "Apache Tomcat 11.0 %SERVICE_NAME%" ^
     --Install "%EXECUTABLE%" ^
     --LogPath "%CATALINA_BASE%\logs" ^
